bc21da12e088f5a8c4ef1e9bbf74522b7e345316
Tim Gatzky <info@tim-gatzky.de>
2017-07-07 11:53:24 +0200

    initial

---
+++ system/modules/pct_system_modules_check/dca/tl_content.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @copyright	Tim Gatzky 20147, Premium Contao Webworks, Premium Contao Themes
+ * @author		Tim Gatzky <info@tim-gatzky.de>
+ * @package		pct_system_modules_check
+ * @link		http://contao.org
+ * @license     LGPL
+ */
+
+print('<p style="color:green">Perfekt! Your Contao ('.VERSION.') loads external dca files correctly!</p>');
